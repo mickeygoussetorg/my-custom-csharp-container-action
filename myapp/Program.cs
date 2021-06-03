be207b6038bb0f08705033ffb0826d1007e9b993
Mickey Gousset <mickeygousset@github.com>
2021-06-02 21:19:26 -0500

    initial version

---
+++ myapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
